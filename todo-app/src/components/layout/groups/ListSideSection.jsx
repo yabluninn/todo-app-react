8dc14d2ad833c0db67551d8cc0245ee9a2803653
yabluninn <yablunin.artem@gmail.com>
2025-03-31 13:41:40 +0300

    Implement adaptive design for "List Side Section" component

--- todo-app/src/components/layout/groups/ListSideSection.jsx
+++ todo-app/src/components/layout/groups/ListSideSection.jsx
@@ -4,6 +4,8 @@ import EditInput from "../../ui/EditInput";
 import {LIST_TYPES} from "../../../constants/list-types.js";
 import {useListsContext} from "../../../contexts/ListsContext.jsx";
 
+import "../../../styles/tasks/TaskSideSection.css"
+
 export default function ListSideSection({ list, listType, onClose }) {
   const { updateTaskList, updateNoteList } = useListsContext();
 
@@ -45,161 +47,49 @@ export default function ListSideSection({ list, listType, onClose }) {
   }
 
   return (
-      <div style={styles.container}>
-        <div style={styles.contentWrapper}>
-          <div style={styles.header}>
-            <div style={styles.headerSubblock}>
-              <i
-                  className="hgi-stroke hgi-arrow-right-double"
-                  style={styles.headerIcon}
-              ></i>
-              <p style={styles.headerListName}>{listType} list</p>
+      <div className="task-side-container">
+        <div className="task-side-content">
+          <div className="task-side-header">
+            <div className="task-side-header-subblock">
+              <i className="hgi-stroke hgi-arrow-right-double task-side-header-icon"></i>
+              <p className="task-side-header-listname">{listType} list</p>
             </div>
-            <button style={styles.closeButton} onClick={onClose}>
-              <i className="hgi-stroke hgi-cancel-01" style={styles.closeIcon}></i>
+            <button className="task-side-close-button" onClick={onClose}>
+              <i className="hgi-stroke hgi-cancel-01 task-side-close-icon"></i>
             </button>
           </div>
-          <div style={styles.inputBlock}>
-            <div style={styles.infoSubBlock}>
+          <div className="task-side-info-block">
+            <div className="task-side-info-subblock">
               <EditInput
                   type="text"
                   placeholder="List name..."
                   value={newListName}
                   onChange={(e) => setNewListName(e.target.value)}
               />
-              <p style={styles.infoLabel}>Name</p>
+              <p className="task-side-info-label">Name</p>
             </div>
-            <div style={styles.infoSubBlock}>
+          </div>
+          <div className="task-side-info-block">
+            <div className="task-side-info-subblock">
               <EditInput
                   type="color"
                   placeholder="Set list color"
                   value={newListColor}
                   onChange={(e) => setNewListColor(e.target.value)}
               />
-              <p style={styles.infoLabel}>Color</p>
+              <p className="task-side-info-label">Color</p>
             </div>
           </div>
 
-          <p style={isDefaultList() ? styles.errorLabel : {display: "none"}}>Default lists are not allowed to edit!</p>
+          <p className={isDefaultList() ? "error-label" : "label-hidden"}>Default lists are not allowed to edit!</p>
           <button
-              style={isDefaultList() ? styles.saveButtonDisabled : styles.saveButton}
+              className={isDefaultList() ? "save-button-disabled" : "task-side-save-button"}
               onClick={saveList}
               disabled={isDefaultList()}
           >
             Save
           </button>
-
         </div>
       </div>
   );
 }
-
-const styles = {
-  container: {
-    position: "absolute",
-    top: "0",
-    right: "0",
-    width: "30%",
-    height: "100vh",
-    background: "white",
-    boxShadow: "rgba(99, 99, 99, 0.2) 0px 8px 16px 0px",
-    padding: "18px",
-  },
-  contentWrapper: {
-    display: "flex",
-    height: "100%",
-    flexDirection: "column",
-    flexGrow: 1,
-  },
-  header: {
-    width: "100%",
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
-    flexDirection: "row",
-    marginBottom: "16px",
-  },
-  headerIcon: {
-    fontSize: "16px",
-    fontWeight: "bold",
-    color: "#aaa",
-  },
-  headerListName: {
-    fontSize: "18px",
-    fontWeight: "bold",
-    color: "#333",
-    marginLeft: "8px",
-  },
-  headerSubblock: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    flexDirection: "row",
-  },
-  closeButton: {
-    backgroundColor: "transparent",
-    border: "none",
-    cursor: "pointer",
-  },
-  closeIcon: {
-    fontSize: "20px",
-    color: "#888",
-  },
-  inputBlock: {
-    width: "100%",
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
-    flexDirection: "column",
-    marginTop: "16px",
-    paddingBottom: "16px",
-    gap: "24px",
-    borderBottom: "1px solid #ccc",
-  },
-  infoSubBlock: {
-    width: "100%",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    flexDirection: "column",
-  },
-  infoLabel: {
-    fontSize: "14px",
-    color: "grey",
-    marginTop: "6px",
-  },
-  saveButton: {
-    backgroundColor: "#7437ff",
-    color: "white",
-    fontSize: "16px",
-    fontWeight: "bold",
-    borderRadius: "8px",
-    width: "100%",
-    height: "40px",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    marginTop: "auto",
-  },
-  saveButtonDisabled: {
-    backgroundColor: "#aaa",
-    color: "#ddd",
-    cursor: "not-allowed",
-    opacity: 0.6,
-    fontSize: "16px",
-    fontWeight: "bold",
-    borderRadius: "8px",
-    width: "100%",
-    height: "40px",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    marginTop: "auto",
-  },
-  errorLabel: {
-    width: "100%",
-    textAlign: "center",
-    color: "#cc1010",
-    marginTop: "auto",
-  }
-};
