489e435060d19292c00a23c875d491df4b083e9c
yabluninn <yablunin.artem@gmail.com>
2025-04-09 20:23:53 +0300

    Add translations for "Notifications" components for ua_UA

--- todo-app/src/components/layout/notifications/NotificationComponent.jsx
+++ todo-app/src/components/layout/notifications/NotificationComponent.jsx
@@ -1,7 +1,10 @@
 import {useEffect, useState} from "react";
+import {useTranslation} from "react-i18next";
 
 export default function NotificationComponent ({ task, removeNotification }) {
     if (!task) return null;
+
+    const { t } = useTranslation();
 
     const [timeAgo, setTimeAgo] = useState("");
 
@@ -21,21 +24,21 @@ export default function NotificationComponent ({ task, removeNotification }) {
             if (hours > 0) result += `${hours} hours `;
             if (minutes > 0) result += `${minutes} minutes `;
 
-            setTimeAgo(result.trim() || "just now");
+            setTimeAgo(result.length > 0 ? result.join(" ") : t("just_now"));
         };
 
         updateTimeAgo();
         const interval = setInterval(updateTimeAgo, 60000);
 
         return () => clearInterval(interval);
-    }, [task.timestamp]);
+    }, [task.timestamp, t]);
 
     return (
         <div className="notification-item">
             <div className="notification-info">
-                <p className="notification-title">{task.type === "start" ? "It's time to start the task!" : "You have 5 minutes left!"}</p>
-                <p className="notification-time">Task: {task.name}</p>
-                <p className="notification-received">{timeAgo} ago</p>
+                <p className="notification-title">{task.type === "start" ? t("notification_start") : t("notification_end")}</p>
+                <p className="notification-time">{t("notification_task", { name: task.name })}</p>
+                <p className="notification-received">{t("notification_ago", { time: timeAgo })}</p>
             </div>
             <button className="notification-remove-btn" onClick={() => removeNotification(task._id)}>
                 <i className="hgi-stroke hgi-delete-02"></i>
