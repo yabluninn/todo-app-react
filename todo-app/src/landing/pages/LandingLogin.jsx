32e683f5c184f6009f3061034f93bc4b04d4ba14
yabluninn <yablunin.artem@gmail.com>
2025-04-11 13:40:58 +0300

    Implement "Landing Sign In" page adaptive design

--- todo-app/src/landing/pages/LandingLogin.jsx
+++ todo-app/src/landing/pages/LandingLogin.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom"; // Импорт для редиректа
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { Link } from "react-router-dom";
 
